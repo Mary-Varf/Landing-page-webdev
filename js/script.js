46e75614161456043e78c30c8ba361386150954e
Mary-Varf <67269361+Mary-Varf@users.noreply.github.com>
2021-01-09 17:11:22 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,195 @@
+var ready;
+
+ready = function() {
+$(function () {
+	//script for popups
+	$("button.button").click(function () {
+		$("div."+$(this).attr("rel")).fadeIn(500);
+		$("body").append("<div id='overlay'></div>");
+        $("#overlay").css({
+            "width":"100%",
+            "height":"100%",
+            "position":"fixed",
+            "top":"0",
+            "left":"0",
+            "display":"none",
+            "background": "#000",
+            "z-index": "50",
+            "opacity": ".50"
+        });
+		$("#overlay").show().css({"filter" : "alpha(opacity=80)"});
+		return false;				
+	});	
+	$("a.close").click(function () {
+		$(this).parent().fadeOut(100);
+		$("#overlay").remove("#overlay");
+		return false;
+	});
+	//script for tabs
+	$("div.selectTabs").each(function () {
+		var tmp = $(this);
+		$(tmp).find(".lineTabs li").each(function (i) {
+			$(tmp).find(".lineTabs li:eq("+i+") a").click(function(){
+				var tab_id=i+1;
+				$(tmp).find(".lineTabs li").removeClass("active");
+				$(this).parent().addClass("active");
+				$(tmp).find(".tab_content div").stop(false,false).hide();
+				$(tmp).find(".tab"+tab_id).stop(false,false).fadeIn(300);
+				return false;
+			});
+		});
+	});
+});
+$(document).click( function(e){
+    if ( $(e.target).closest(".popup").length ) {// клик внутри элемента 
+        return;
+    }
+    // клик снаружи элемента 
+    $(".popup").fadeOut();
+    $("#overlay").fadeOut();
+
+});
+
+
+window.onload = function() {
+	let header__burger = document.querySelector('.header__burger');
+	let header_nav = document.querySelector('.header__nav');
+	let header__list = document.querySelector('.header__list');
+	
+	header__burger.onclick = function(){
+		header__burger.classList.toggle('active');
+		header_nav.classList.toggle('active');
+	}
+	
+	header__list.onclick = function () {
+		header__list.classList.remove('active');
+	}
+	let header__link = document.querySelector('.header__list');
+	
+	header__link.onclick = function () {
+		header__burger.classList.toggle('active');
+		header_nav.classList.toggle('active');
+	}
+}
+$(function(){
+	$(".phone").mask("8(999) 999-9999");
+  });
+
+$(document).ready(function(){
+	$.validator.addMethod("regexp", function(value, element, params) {
+		var expression = new RegExp(params);
+		return this.optional(element) || expression.test(value);
+	});
+		//====== валидация формы =============
+		$(".form-item-1").each(function () {
+						$(this).validate({
+							
+							focusInvalid: false,
+							rules: {
+								name: {
+									required: true,
+									minlength: 2,
+									maxlength: 22,
+									regexp: /^[A-Za-zА-Яа-яЁё\s]/,
+								},
+								phone: {
+									required: true,
+								},
+							},
+							messages: {
+								name: {
+									required: "Заполните поле",
+									minlength:"Введите не менее 2 символов",
+									maxlength: "Введите не более 22 символов",
+									regexp: "Используйте только русские <br>и английские буквы",
+								},
+								phone: {
+									required: "Заполните поле",
+								},
+							},
+							submitHandler(form) {
+								let th = $(form);
+	
+								$.ajax({
+								type: "POST",
+								url: "mail.php", //Change
+								data: th.serialize(),
+							}).done(() => {
+								console.log("Отправлено");
+								$(".reg_form__thanks-item-1").css({"display": "block"});
+								setTimeout(function() {
+									$(".popup_success_disappear-item-1").remove();
+									$(".reg_form__success-page-item-1").css({"display": "block", "width":"220px"});	
+								}, 1000);
+								setTimeout(function(){
+									$("#popup-item-1").fadeOut();
+									  $("#overlay").fadeOut();
+								}, 3000);
+						});
+	
+						return false;
+						}
+					});
+			});
+			//====== валидация формы =============
+		$(".form-item-2").each(function () {
+					$(this).validate({
+						
+						focusInvalid: false,
+						rules: {
+							name: {
+								required: true,
+								minlength: 2,
+								maxlength: 22,
+								regexp: /^[A-Za-zА-Яа-яЁё\s]/,
+							},
+							phone: {
+								required: true,
+							},
+							email: {
+								required: true,
+							},
+						},
+						messages: {
+							name: {
+								required: "Заполните поле",
+								minlength:"Введите не менее 2 символов",
+								maxlength: "Введите не более 22 символов",
+								regexp: "Используйте только русские и английские буквы",
+							},
+							phone: {
+								required: "Заполните поле",
+							},
+							email: {
+								required: "Заполните поле",
+							},
+						},
+						submitHandler(form) {
+							let th = $(form);
+	
+							$.ajax({
+							type: "POST",
+							url: "mail.php", //Change
+							data: th.serialize(),
+						}).done(() => {
+							console.log("Отправлено");
+							$(".reg_form__thanks-item-2").css({"display": "block"});
+							setTimeout(function() {
+								$(".popup_success_disappear-item-2").remove();
+								$(".reg_form__success-page-item-2").css({"display": "block", "width":"220px"});	
+							}, 1000);
+							setTimeout(function(){
+								$("#popup-item-2").fadeOut();
+								  $("#overlay").fadeOut();
+							}, 3000);
+					});
+					return false;
+					}
+				});
+			});
+	})
+};
+
+$(document).ready(ready);
+
+$(document).on('page:load', ready);
